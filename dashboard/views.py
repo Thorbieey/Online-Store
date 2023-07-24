b3005ae858061a68a6b48291fa05739f3add4d32
Tobi Olajide <tobiolajide887@yahoo.com>
2023-07-24 16:43:56 +0100

    edit added- not currently updating instance

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from item.models import Item
+from item.models import Item, Category
  
 @login_required   
 def index(request):
     items = Item.objects.filter(created_by = request.user)
+    categories = Category.objects.all()
     
     return render(request, 'dashboard/index.html', {
         'items': items,
+        'categories': categories,
     })
